a42c6f03e75a10678dc9c3ac9b15f2bfdb569e9e
sheyxov <cavansirsheyxov@gmail.com>
2025-11-30 10:40:04 +0000

    salam

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module to search MongoDB schools by topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns a list of schools having a specific topic
+    Args:
+        mongo_collection: pymongo collection object
+        topic (string): topic searched
+    """
+    return list(mongo_collection.find({ "topics": topic }
